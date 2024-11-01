756cd083a330b2e0f85e88fee2687c0473b1e6e5
vinicius-oliveiracr <vinicius.cr.o.3@gmail.com>
2024-11-01 11:00:27 -0300

    implementando task views

--- api_erp/companies/urls.py
+++ api_erp/companies/urls.py
@@ -4,12 +4,16 @@ from companies.views.employees import Employees, EmployeeDetail
 
 from companies.views.permissions import PermissionDetail
 
+from companies.views.groups import Groups, GroupDetail
+
 urlpatterns = [
     
     path('employees', Employees.as_view()),
-
     path('employees/<int:employee_id>', EmployeeDetail.as_view()),
 
 
-    path('permissions',PermissionDetail.as_view())
+    path('permissions',PermissionDetail.as_view()),
+
+    path('groups', Groups.as_view()),
+    path('groups/<int:group_id>', GroupDetail.as_view()),
 ]
